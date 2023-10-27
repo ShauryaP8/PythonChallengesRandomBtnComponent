51c1a938dc18259414f4dd69b7b66606cae5efd4
ShauryaP8 <143507334+ShauryaP8@users.noreply.github.com>
2023-10-27 20:21:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const generateButton = document.getElementById('generateButton');
     const randomQuestion = document.getElementById('randomQuestion');
 
-    // Load the CSV file and parse it into an array of questions
-    fetch('questions.csv')
+    // Load the TSV file and parse it into an array of questions
+    fetch('questions.tsv')
         .then(response => response.text())
         .then(data => {
             const questionsArray = data.split('\n').filter(question => question.trim() !== '');
@@ -14,22 +14,22 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Remove unwanted parts
                 randomQ = randomQ
                     .replace('GIS SUBMIT>>', '') // Remove "GIS SUBMIT>>"
-                    .replace(/,*$/, '') // Remove trailing commas
+                    .replace(/\t+$/, ''); // Remove trailing tabs
 
-                // Extract the URL and description
-                const parts = randomQ.split(',');
+                // Split the TSV line by tabs
+                const parts = randomQ.split('\t');
                 const challengeCode = parts[0];
                 const score = parts[1];
                 const challenge = parts[2];
-                const description = parts.slice(3).join(', ');
+                const description = parts.slice(3).join('\t');
 
-                // Format the output with labels and links
-                randomQ = `Challenge Code: ${challengeCode}\nScore: ${score}\nChallenge: ${challenge}\nDescription: ${description}`;
+                // Format the output with labels and line breaks
+                randomQ = `Challenge Code:\n${challengeCode}\nScore:\n${score}\nChallenge:\n${challenge}\nDescription:\n${description}`;
 
                 randomQuestion.innerHTML = randomQ;
             });
         })
         .catch(error => {
-            console.error('Error loading CSV file:', error);
+            console.error('Error loading TSV file:', error);
         });
 });
